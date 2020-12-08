becd754bf2a372844f0e18acfb625c0e22109150
ghostop14 <ghostop14@gmail.com>
2020-12-07 19:41:40 -0500

    Initial commit

---
+++ include/xcorrelate/xcomplexstruct.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_XCORRELATE_XCOMPLEXSTRUCT_H_
+#define INCLUDE_XCORRELATE_XCOMPLEXSTRUCT_H_
+
+struct XComplexStruct {
+	float real = 0.0f;
+	float imag = 0.0f;
+};
+typedef struct XComplexStruct XComplex;
+
+#endif /* INCLUDE_XCORRELATE_XCOMPLEXSTRUCT_H_ */
