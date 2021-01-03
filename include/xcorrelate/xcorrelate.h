38c598da97337978812ca22a1fad69d5fc9c542d
ghostop14 <ghostop14@gmail.com>
2021-01-03 12:40:48 -0500

    Updated async parameter

--- include/xcorrelate/xcorrelate.h
+++ include/xcorrelate/xcorrelate.h
@@ -50,7 +50,7 @@ namespace gr {
        * \param normalize Perform normalization on the calculated correlation
        */
       static sptr make(int num_inputs, int signal_length, int data_type, int data_size, int max_search_index,
-    		  int decim_frames, int num_outputs, bool async=false, bool normalize=true);
+    		  int decim_frames, int num_outputs, bool basync=false, bool normalize=true);
     };
 
   } // namespace xcorrelate
